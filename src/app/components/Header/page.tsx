cb97210e4b40f4e7f618cb79af5d01de181f6b4a
Arya Gupta <aryagupta239@gmail.com>
2025-09-26 23:50:14 +0530

    new favicon.ico added

--- src/app/components/Header/page.tsx
+++ src/app/components/Header/page.tsx
@@ -1,13 +1,28 @@
-import {FC} from 'react'
+"use client";
+
+import {FC, useState} from 'react'
 import Image from 'next/image';
 import header_image from '@/../public/images/logo-site.png'
 import menu from '@/../public/images/menu.png'
 
 const Header : FC = () => {
+    const [open, setOpen] = useState<boolean>(false);
+
     return(
-        <div className='grid grid-cols-3 gap-4'>
-            <Image src={menu} alt='menu' className='w-20'/>
-            <Image className='col-span-2' src={header_image} alt='header_image' />
+        <div className='grid grid-cols-3 gap-4 p-5'>
+            <Image className='w-13 pl-3 pt-5' src={menu} alt='menu'/>
+            <div className='col-span-2 w-30' onClick={() => setOpen(!open)}>
+                <Image src={header_image} alt='header_image' />
+            </div>
+            {open && 
+                (<div>
+                    <a href="">SHOP</a>
+                    <a href="">INSPIRATION</a>
+                    <a href="">CONTACT</a>
+                    <a href="">SEARCH</a>
+                    <a href="">ACCOUNT</a>
+                    <a href="">CART</a>
+                </div>) }
         </div>
     )
 }
@@ -15,14 +30,3 @@ const Header : FC = () => {
 export default Header;
 
 
-{/* <>
-            <ul className='flex justify-between p-3 text-xs items-center'>
-                <li>SHOP</li>
-                <li>INSPIRATION</li>
-                <li>CONTACT</li>
-                <li><Image className='p-1 w-100 h-auto' src={header_image} alt='header_image' /></li>
-                <li>SEARCH</li>
-                <li>ACCOUNT</li>
-                <li>CART</li>
-            </ul>
-        </> */}
