c85b1c33e988820fc76b6cfda63bc9d05f8e0306
Arya Gupta <aryagupta239@gmail.com>
2025-09-26 23:19:39 +0530

    globals.css edited

--- src/app/components/Header/page.tsx
+++ src/app/components/Header/page.tsx
@@ -1,12 +1,14 @@
 import {FC} from 'react'
 import Image from 'next/image';
 import header_image from '@/../public/images/logo-site.png'
+import menu from '@/../public/images/menu.png'
 
 const Header : FC = () => {
     return(
         <div className='grid grid-cols-3 gap-4'>
-
-        </>
+            <Image src={menu} alt='menu' className='w-20'/>
+            <Image className='col-span-2' src={header_image} alt='header_image' />
+        </div>
     )
 }
 
