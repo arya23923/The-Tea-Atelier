3298981f3b0b987c17294d19a76fa519c0c1eb49
Arya Gupta <aryagupta239@gmail.com>
2025-09-30 22:03:37 +0530

    fully functioning header with proper animations for when scrolled and for when hovered in different situations

--- src/app/components/Header/page.tsx
+++ src/app/components/Header/page.tsx
@@ -23,7 +23,7 @@ const Header : FC = () => {
 
     useEffect(() => {
         const handlescroll = () => {
-            if(window.scrollY > 30){
+            if(window.scrollY > 50){
                 setScroll(true);
                 setLogo("/images/logo-site.png");
                 setSearch("/images/search.png");
@@ -53,31 +53,37 @@ const Header : FC = () => {
                 <a href="#" className='inline'><Image src={cart} alt='cart image' className='w-15 h-8 self-center pr-3 ml-5 pl-3 mt-10' /></a>
                 <a href="#" className='inline'><Image src={search_icon} className='w-12 h-10 self-center p-2 pr-3 pl-3 mt-10' alt='search'/></a>
             </div>
-            <div className={`hidden md:flex justify-around items-center p-3 text-white backdrop-blur-xl fixed w-screen z-40 hover:text-black hover:bg-white `} onMouseOver={() => {setLogo("/images/logo-site.png"); setSearch("/images/search.png")}} onMouseOut={() => {setLogo("/images/inverted-logo.png"); setSearch("/images/invert-search.png")}}>
+            <div className={`hidden md:flex justify-around items-center p-3 text-white backdrop-blur-xl fixed w-screen z-40 group hover:bg-white ${scroll ? "bg-white" : "backdrop-blur-xl"}`} onMouseOver={() => {setLogo("/images/logo-site.png"); setSearch("/images/search.png")}} onMouseOut={() => {
+                if(scroll){
+                    setLogo("/images/logo-site.png"); setSearch("/images/search.png"); 
+                } else {
+                    setLogo("/images/inverted-logo.png"); setSearch("/images/invert-search.png");
+                }
+            }}>
                 <a href="#" className={`relative after:content-[''] after:absolute after:left-0 after:bottom-0
                     after:w-0 after:h-[2px] after:bg-black
-                    after:transition-all after:duration-300 hover:after:w-full ${scroll ? "text-black" : "text-white"}`}>SHOP</a>
+                    after:transition-all after:duration-300 hover:after:w-full ${scroll ? "text-black" : "text-white"} group-hover:text-black`}>SHOP</a>
                 <a href="#" className={`relative after:content-[''] after:absolute after:left-0 after:bottom-0
                     after:w-0 after:h-[2px] after:bg-black
-                    after:transition-all after:duration-300 hover:after:w-full ${scroll ? "text-black" : "text-white"}`}>INSPIRATION</a>
+                    after:transition-all after:duration-300 hover:after:w-full ${scroll ? "text-black" : "text-white"} group-hover:text-black`}>INSPIRATION</a>
                 <a href="#" className={`relative after:content-[''] after:absolute after:left-0 after:bottom-0
                     after:w-0 after:h-[2px] after:bg-black
-                    after:transition-all after:duration-300 hover:after:w-full ${scroll ? "text-black" : "text-white"}`}>CONTACT</a>
+                    after:transition-all after:duration-300 hover:after:w-full ${scroll ? "text-black" : "text-white"} group-hover:text-black`}>CONTACT</a>
                 <Link href="/components/Homepage" className={`relative after:content-[''] after:absolute after:left-0 after:bottom-0
                     after:w-0 after:h-[2px] after:bg-black
                     after:transition-all after:duration-300 hover:after:w-full ${scroll ? "text-black" : "text-white"}`}><Image src={logo} width={170} height={150} alt='header_image'/></Link>
                 <a href="#" className={`relative after:content-[''] after:absolute after:left-0 after:bottom-0
                     after:w-0 after:h-[2px] after:bg-black
-                    after:transition-all after:duration-300 hover:after:w-full ${scroll ? "text-black" : "text-white"} flex`}>
+                    after:transition-all after:duration-300 hover:after:w-full ${scroll ? "text-black" : "text-white"} flex group-hover:text-black`}>
                     <p className=''>SEARCH</p>
                     <Image src={search} width={10} height={10} className='w-7 p-1.5 pt-1' alt='search'/>
                 </a>
                 <a href="#" className={`relative after:content-[''] after:absolute after:left-0 after:bottom-0
                     after:w-0 after:h-[2px] after:bg-black
-                    after:transition-all after:duration-300 hover:after:w-full ${scroll ? "text-black" : "text-white"}`}>ACCOUNT</a>
+                    after:transition-all after:duration-300 hover:after:w-full ${scroll ? "text-black" : "text-white"} group-hover:text-black`}>ACCOUNT</a>
                 <a href="#" className={`relative after:content-[''] after:absolute after:left-0 after:bottom-0
                     after:w-0 after:h-[2px] after:bg-black
-                    after:transition-all after:duration-300 hover:after:w-full ${scroll ? "text-black" : "text-white"}`}>CART</a>
+                    after:transition-all after:duration-300 hover:after:w-full ${scroll ? "text-black" : "text-white"} group-hover:text-black`}>CART</a>
             </div>
         </header>
     )
