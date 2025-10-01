a24eeab08568cceb3364b04ac620a60532e5891a
Arya Gupta <aryagupta239@gmail.com>
2025-10-01 23:10:38 +0530

    filter sidebar except ranger works on phone version

--- src/app/components/shop/filter.tsx
+++ src/app/components/shop/filter.tsx
@@ -7,6 +7,13 @@ import "react-range-slider-input/dist/style.css";
 import Image from "next/image";
 import cross from '@/../public/images/cross.png'
 
+import { Montserrat } from 'next/font/google'
+ 
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "700"], 
+});
+
 interface prop{
     isOpen : boolean;
     onClose : () => void;
@@ -18,12 +25,30 @@ const Filter:FC<prop> = ({isOpen, onClose, onApply}) => {
     const [max, setMax] = useState<number>(150);
 
     const apply = () => {
-        onApply(min, max)
+        onApply(min, max);
+        onClose();
     };
 
     return(
-       <div className={`fixed inset-0 transition-all duration-300 z-50 w-70 bg-white p-5 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
+       <div className={`fixed inset-y-0 left-0 bottom-0 mt-40 transition-all duration-300 z-10 w-90 md:w-170 text-black bg-amber-100 p-5 ${montserrat.className} ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
             <div onClick={onClose}><Image src={cross} alt="cross" className="w-10 h-auto justify-self-end"/></div>
+            <div className="p-5 justify-center">
+                <p className="p-5 text-xl">Price</p>
+                <RangeSlider
+                    className="single-thumb"
+                    defaultValue={[0, 150]}
+                    thumbsDisabled={[true, false]}
+                    rangeSlideDisabled={true}
+                    min={0}
+                    max={150}
+                />
+                <div className="flex flex-col p-5 pb-15 pt-10 md:flex-row">
+                    <label className="text-lg">₹ <input type="number" value={min} onChange={(e) => setMin(Number(e.target.value))} className="border border-black w-50 pr-5 pl-5 pt-3 pb-3 text-lg"/></label>
+                    <p className="text-2xl p-5">-</p>
+                    <label className="text-lg">₹ <input type="number" value={max} onChange={(e) => setMax(Number(e.target.value))} className="border border-black w-50 pr-5 pl-5 pt-3 pb-3 text-lg"/></label>   
+                </div>
+                <button className="flex justify-center p-5 border ml-10 w-50 text-white border-amber-900 bg-amber-900" onClick={apply}>Apply</button>
+            </div>
        </div>
     )
 }
