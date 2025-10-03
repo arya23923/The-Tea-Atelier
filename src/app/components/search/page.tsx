ff8301eb723f7e1af5b4dd94b9130652d75139a7
Arya Gupta <aryagupta239@gmail.com>
2025-10-03 22:51:02 +0530

    skeleton search made with no content

---
+++ src/app/components/search/page.tsx
@@ -0,0 +1,55 @@
+import{FC, useEffect, useState} from "react"
+
+interface Tea{
+    id: number,
+    name: string,
+    price: number,
+    image: string,
+    description : string;
+}
+
+interface SearchButton {
+    isOpen : boolean,
+    isClose : () => void;
+}
+
+const SearchComp:FC<SearchButton> = ({isOpen, isClose}) => {
+    const [tea, setTea] = useState<Tea[]>([]);
+    const [load, setLoad] = useState<boolean>(true);
+    const [sorted, setSorted] = useState<Tea[]>([]);
+
+    useEffect(() => {
+        const fetchMatch = async () => {
+            try{
+                const res = await fetch('/api/tea');
+                const data:Tea [] = await res.json();
+                setTea(data);
+            } catch(error) {
+                console.error("could not fetch teas : ",error);
+            } finally {
+                setLoad(false);
+            }
+        }
+        fetchMatch();
+    }, [])
+
+    if(load) return(<>Loading....</>)
+
+    const match = (name_match : string) => {
+        if (!tea) return;
+
+        const matchedTeas = tea.filter((t) =>
+            t.name.toLowerCase().includes(name_match.toLowerCase()) 
+        );
+
+        setSorted(matchedTeas);
+    }
+
+    return(
+        <div className= {`transition-all h-screen bg-white ${isOpen ? "translate-y-0" : "-translate-y-full"} `}>
+            <p>Hello</p>
+        </div>
+    )
+}
+
+export default SearchComp;
