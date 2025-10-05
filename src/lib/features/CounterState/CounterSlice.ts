e0b001cd9ac1d6468ef02b9b91bb49407c5b4732
Arya Gupta <aryagupta239@gmail.com>
2025-10-05 21:18:30 +0530

    updated CounterSlice.ts

--- src/lib/features/CounterState/CounterSlice.ts
+++ src/lib/features/CounterState/CounterSlice.ts
@@ -2,29 +2,54 @@
 
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
+
+interface Tea{
+  id: number,
+  name: string,
+  price: number,
+  image: string,
+  description : string,
+  quantity : number
+}
+
 export interface CounterState {
   value: number;
+  cart : Tea[],
 }
 const initialState: CounterState = {
   value: 0,
+  cart : [],
 };
+
 export const CounterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
-    increment: (state, action: PayloadAction<number | undefined>) => {
-      const incrementBy = action.payload !== undefined ? action.payload : 1;
-      state.value += incrementBy;
+    addItem: (state, action: PayloadAction<Tea>) => {
+      const {id, name, price, image, description} = action.payload;
+      const existing = state.cart.find((tea) => tea.id === id)
+      if(existing) existing.quantity++;
+      else state.cart.push({id, name, price, image, description, quantity : 1})
+      state.value++;
     },
-    decrement: (state, action: PayloadAction<number | undefined>) => {
-      const incrementBy = action.payload !== undefined ? action.payload : 1;
-      state.value -= incrementBy;
+    removeItem: (state, action: PayloadAction<Tea>) => {
+      const {id, name, price, image, description} = action.payload;
+      const existing = state.cart.find(tea => tea.id === id);
+      if(existing){
+        state.cart = state.cart.filter(tea => tea.id !== id);
+        state.value -= existing.quantity;
+      }
     },
-    resetState: (state) => {
-      state.value = 0;
+    updateQuantity: (state, action : PayloadAction<{id: number, quantity : number}>) => {
+      const existing = state.cart.find(tea => tea.id === action.payload.id)
+      if(existing){
+        const difference = action.payload.quantity - existing.quantity;
+        state.value += difference;
+        existing.quantity = action.payload.quantity;
+      }
     },
   },
 });
 // Action creators are generated for each case reducer function
-export const { increment, decrement, resetState } = CounterSlice.actions;
+export const { addItem, removeItem, updateQuantity } = CounterSlice.actions;
 export default CounterSlice.reducer;
