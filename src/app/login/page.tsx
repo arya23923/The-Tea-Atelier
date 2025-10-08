5124c893b1b6a3540d5dcf77146e5609d31af00b
Arya Gupta <aryagupta239@gmail.com>
2025-10-08 23:38:07 +0530

    added functionality to log in as well

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,8 @@
-import {FC} from "react"
+import {FC, useState} from "react"
 import Image from "next/image";
 import loginImage from '@/../public/images/login.jpg'
 import google from '@/../public/images/google.png'
+import { signIn } from "next-auth/react";
 
 import { Montserrat } from 'next/font/google'
 const montserrat = Montserrat({
@@ -11,6 +12,24 @@ const montserrat = Montserrat({
 
 
 const loginPage : FC = () => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const res = await signIn("credentials", {
+            redirect: false,
+            email,
+            password,
+        });
+
+        if (res?.error) {
+            alert(res.error);
+        } else {
+            window.location.href = "/";
+        }
+    };
     return(
         <div className={`${montserrat.className} flex`}>
             <Image src={loginImage} alt="sign in image" className="hidden md:block h-screen w-auto"/>
@@ -18,9 +37,9 @@ const loginPage : FC = () => {
                 <p className="text-xl md:text-5xl">Hello,</p>
                 <p className="text-xl md:text-5xl">Welcome back!</p>
                 <form action="" className="flex flex-col space-y-4">
-                    <input type="email" placeholder="Enter email address" className="border border-gray-400 pt-3 pb-3 pr-5 pl-5 rounded-sm bg-gray-300 md:w-100 md:mt-10 text-black"></input>
-                    <input type="password" placeholder="Enter password" className="border border-gray-400 pt-3 pb-3 pr-5 pl-5 rounded-sm bg-gray-300 md:w-100 text-black"></input>
-                    <button className="pt-3 pb-3 pr-5 pl-5 bg-blue-600 text-white rounded-sm">Log In</button>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter email address" className="border border-gray-400 pt-3 pb-3 pr-5 pl-5 rounded-sm bg-gray-300 md:w-100 md:mt-10 text-black"></input>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}  placeholder="Enter password" className="border border-gray-400 pt-3 pb-3 pr-5 pl-5 rounded-sm bg-gray-300 md:w-100 text-black"></input>
+                    <button className="pt-3 pb-3 pr-5 pl-5 bg-blue-600 text-white rounded-sm" onClick={handleLogin}>Log In</button>
                 </form>
                 <p className="hover:underline hover:cursor-pointer md:text-xl md:mt-5">Not registered ? Create an account</p>
                 <button className="flex w-60 justify-center p-5 space-x-3 border border-gray-800 pt-3 pb-3 pr-5 pl-5 rounded-sm mt-10 md:mt-5">
