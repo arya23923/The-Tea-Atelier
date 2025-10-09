78536aaabdf62d9a38600aa6e05723e70ad7e664
Arya Gupta <aryagupta239@gmail.com>
2025-10-09 19:49:22 +0530

    removed serversession in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import StoreProvider from "./StoreProvider";
 import Header from './components/Header/page'
-import { getServerSession } from "next-auth";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
