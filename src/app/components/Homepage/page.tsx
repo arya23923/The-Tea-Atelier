fb92e3a6a457e5b9a16b2fcb36d6ad5008e26a41
Arya Gupta <aryagupta239@gmail.com>
2025-10-09 19:46:17 +0530

    changed single quotations to &apos; bs

--- src/app/components/Homepage/page.tsx
+++ src/app/components/Homepage/page.tsx
@@ -19,7 +19,7 @@ const Home : FC = () => {
         <div className={`bg-[url(/images/home-page.jpg)] bg-cover md:bg-cover w-screen h-screen ${inriaSerif.className}`}>
             <div className="absolute inset-0 bg-white/10 z-0"></div>
             <p className='relative z-10 text-black flex justify-center items-center w-100 text-xl p-5 pt-25 md:text-2xl md:w-4xl md:justify-end md:items-end md:ml-20 md:pt-150'>The Tea Atelier brings you handcrafted blends made with love from the lush tea gardens of of various parts of India. 
-                Experience the richness and purity of India's finest, straight from nature to your cup.</p>
+                Experience the richness and purity of India&apos;s finest, straight from nature to your cup.</p>
             <button className="relative z-10 flex justify-center bg-[#5A4D5D] text-white pt-3 pr-5 pl-5 pb-3 ml-5 rounded-lg cursor-pointer md:ml-25 md:text-xl md:mt-5" onClick={handleClick}>EXPLORE</button>
         </div>
     )
